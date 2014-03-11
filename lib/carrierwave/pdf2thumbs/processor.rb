596a6c81247048452ee09ff1744a522736098ddd
Eduardo Martines <martines.eduardo@gmail.com>
2014-03-11 02:14:29 -0300

    rolling back but changing

--- lib/carrierwave/pdf2thumbs/processor.rb
+++ lib/carrierwave/pdf2thumbs/processor.rb
@@ -1,11 +1,10 @@
 module CarrierWave
   module Pdf2thumbs
     class Processor
-      def initialize(input_path, output_path, width, height)
-        @input_path  = input_path
-        @output_path = output_path
-        @width       = width
-        @height      = height
+      def initialize(input_path, width, height)
+        @input_path = input_path
+        @width      = width
+        @height     = height
       end
 
       def process
@@ -15,7 +14,28 @@ module CarrierWave
       private
 
       def extract_images
-        CarrierWave::Pdf2thumbs::ProcessorService.new(@input_path, @output_path, @width, @height).process
+        CarrierWave::Pdf2thumbs::ProcessorService.new(input_path, output_path, width, height).process
+      end
+
+      def input_path
+        @input_path
+      end
+
+      def output_path
+        path = Pathname.new(input_path).dirname
+        File.join(path, folder_name)
+      end
+
+      def folder_name
+        "#{@width}x#{@height}"
+      end
+
+      def width
+        @width
+      end
+
+      def height
+        @height
       end
     end
   end
